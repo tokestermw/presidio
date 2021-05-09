1d74f328b10b9c8a1dc6f131b1701686a6b0c19b
shiranrubin <shiranrubin@microsoft.com>
2021-05-09 10:20:25 +0300

    Swagger generator with .net core 3.1

--- sdk/csharp/build.bat
+++ sdk/csharp/build.bat
@@ -3,15 +3,7 @@
 
 @echo off
 
-SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
-
-if not exist ".\nuget.exe" powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '.\nuget.exe')"
-.\nuget.exe install src\Microsoft.Presidio\packages.config -o packages
-
-if not exist ".\bin" mkdir bin
-
-copy packages\Newtonsoft.Json.10.0.3\lib\net45\Newtonsoft.Json.dll bin\Newtonsoft.Json.dll
-copy packages\JsonSubTypes.1.2.0\lib\net45\JsonSubTypes.dll bin\JsonSubTypes.dll
-copy packages\RestSharp.105.1.0\lib\net45\RestSharp.dll bin\RestSharp.dll
-%CSCPATH%\csc  /reference:bin\Newtonsoft.Json.dll;bin\JsonSubTypes.dll;bin\RestSharp.dll;System.ComponentModel.DataAnnotations.dll  /target:library /out:bin\Microsoft.Presidio.dll /recurse:src\Microsoft.Presidio\*.cs /doc:bin\Microsoft.Presidio.xml
-
+dotnet restore src\Microsoft.Presidio
+dotnet build src\Microsoft.Presidio
+echo Now, run the following to start the project: dotnet run -p src\Microsoft.Presidio\Microsoft.Presidio.csproj --launch-profile web.
+echo.
