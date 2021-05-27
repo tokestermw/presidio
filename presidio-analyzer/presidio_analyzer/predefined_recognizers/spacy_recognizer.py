798a54d5401610fef59f77625d2117e1ca30b348
sharon <sharon.hart@microsoft.com>
2021-05-27 10:32:48 +0300

    add date to spacy

--- presidio-analyzer/presidio_analyzer/predefined_recognizers/spacy_recognizer.py
+++ presidio-analyzer/presidio_analyzer/predefined_recognizers/spacy_recognizer.py
@@ -26,7 +26,7 @@ class SpacyRecognizer(LocalRecognizer):
     is translated into a Presidio entity.
     """
 
-    ENTITIES = ["NRP", "LOCATION", "PERSON",
+    ENTITIES = ["DATE_TIME", "NRP", "LOCATION", "PERSON",
                 "ORGANIZATION", # - Less accurate with the 'en_core_web_lg' model,
                 # can be used with more assurance when using 'en_core_web_trf'.
                 ]
